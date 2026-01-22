9fc562d5c68c270af8cc2221c9232bda1ad0a8fd
Hailey <haileykuo96@gmail.com>
2026-01-22 16:04:27 +0800

    Initial Commit

---
+++ HaileyLanguageAI/HaileyLanguageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaileyLanguageAIApp.swift
+//  HaileyLanguageAI
+//
+//  Created by 郭蕙瑄 on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HaileyLanguageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
